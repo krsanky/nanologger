be846354b53d33f63fe40d79beac3064c3219bc2
paul wisehart <paul@oldcode.org>
2018-12-30 17:18:42 +0000

    Take out settings from when this was part of the wiki code.

--- settings.h
+++ settings.h
@@ -3,8 +3,5 @@
 
 #define SERVER_ENDPOINT "tcp://127.0.0.1:23000"
 #define LOG_PREFIX "LOG"
-#define WIKI_ROOT "content"
-#define NUM_HTTP_PARAMS 10
-#define HTML_NULL_MARKER "21345nullybullypoop123"
 
 #endif
